8839ea67dd8ca39bdd657e63af278670dbb2f1e0
Alex Nathanail <adnathanail@hotmail.co.uk>
2021-11-05 01:07:45 +0000

    Add comments

--- src/parser/build.sh
+++ src/parser/build.sh
@@ -1,5 +1,7 @@
+# Build Python version
 antlr4 -Dlanguage=Python3 -o python Command.g4
+# Build Java version
 antlr4 -o java Command.g4
 javac java/Command*.java
-
+# Test with Java
 (cd java; grun Command command -tokens ../input.txt)
