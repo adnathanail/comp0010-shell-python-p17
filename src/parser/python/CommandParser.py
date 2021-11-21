621e3411348b50fad480c041c354f958ad0a4f56
Bartosz Grabek <bartoszgrabek7@gmail.com>
2021-11-21 14:58:35 +0000

    Update parser/python files to match latest grammar

--- src/parser/python/CommandParser.py
+++ src/parser/python/CommandParser.py
@@ -11,41 +11,42 @@ else:
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\n")
-        buf.write("i\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\13")
+        buf.write("l\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b")
         buf.write("\t\b\4\t\t\t\3\2\3\2\5\2\25\n\2\3\2\5\2\30\n\2\3\2\3\2")
-        buf.write("\3\3\3\3\3\3\5\3\37\n\3\3\4\3\4\3\4\5\4$\n\4\3\5\7\5\'")
-        buf.write("\n\5\f\5\16\5*\13\5\3\5\3\5\7\5.\n\5\f\5\16\5\61\13\5")
-        buf.write("\7\5\63\n\5\f\5\16\5\66\13\5\3\5\3\5\7\5:\n\5\f\5\16\5")
-        buf.write("=\13\5\3\5\7\5@\n\5\f\5\16\5C\13\5\3\5\7\5F\n\5\f\5\16")
-        buf.write("\5I\13\5\3\6\3\6\5\6M\n\6\3\7\3\7\6\7Q\n\7\r\7\16\7R\3")
-        buf.write("\b\3\b\7\bW\n\b\f\b\16\bZ\13\b\3\b\3\b\3\b\7\b_\n\b\f")
-        buf.write("\b\16\bb\13\b\3\b\5\be\n\b\3\t\3\t\3\t\2\2\n\2\4\6\b\n")
-        buf.write("\f\16\20\2\3\3\2\b\t\2p\2\22\3\2\2\2\4\33\3\2\2\2\6 \3")
-        buf.write("\2\2\2\b(\3\2\2\2\nL\3\2\2\2\fP\3\2\2\2\16d\3\2\2\2\20")
-        buf.write("f\3\2\2\2\22\24\5\b\5\2\23\25\5\6\4\2\24\23\3\2\2\2\24")
-        buf.write("\25\3\2\2\2\25\27\3\2\2\2\26\30\5\4\3\2\27\26\3\2\2\2")
-        buf.write("\27\30\3\2\2\2\30\31\3\2\2\2\31\32\7\2\2\3\32\3\3\2\2")
-        buf.write("\2\33\34\7\3\2\2\34\36\5\2\2\2\35\37\5\4\3\2\36\35\3\2")
-        buf.write("\2\2\36\37\3\2\2\2\37\5\3\2\2\2 !\7\4\2\2!#\5\b\5\2\"")
-        buf.write("$\5\6\4\2#\"\3\2\2\2#$\3\2\2\2$\7\3\2\2\2%\'\7\n\2\2&")
-        buf.write("%\3\2\2\2\'*\3\2\2\2(&\3\2\2\2()\3\2\2\2)\64\3\2\2\2*")
-        buf.write("(\3\2\2\2+/\5\16\b\2,.\7\n\2\2-,\3\2\2\2.\61\3\2\2\2/")
-        buf.write("-\3\2\2\2/\60\3\2\2\2\60\63\3\2\2\2\61/\3\2\2\2\62+\3")
-        buf.write("\2\2\2\63\66\3\2\2\2\64\62\3\2\2\2\64\65\3\2\2\2\65\67")
-        buf.write("\3\2\2\2\66\64\3\2\2\2\67A\5\f\7\28:\7\n\2\298\3\2\2\2")
-        buf.write(":=\3\2\2\2;9\3\2\2\2;<\3\2\2\2<>\3\2\2\2=;\3\2\2\2>@\5")
-        buf.write("\n\6\2?;\3\2\2\2@C\3\2\2\2A?\3\2\2\2AB\3\2\2\2BG\3\2\2")
-        buf.write("\2CA\3\2\2\2DF\7\n\2\2ED\3\2\2\2FI\3\2\2\2GE\3\2\2\2G")
-        buf.write("H\3\2\2\2H\t\3\2\2\2IG\3\2\2\2JM\5\16\b\2KM\5\f\7\2LJ")
-        buf.write("\3\2\2\2LK\3\2\2\2M\13\3\2\2\2NQ\5\20\t\2OQ\7\7\2\2PN")
-        buf.write("\3\2\2\2PO\3\2\2\2QR\3\2\2\2RP\3\2\2\2RS\3\2\2\2S\r\3")
-        buf.write("\2\2\2TX\7\5\2\2UW\7\n\2\2VU\3\2\2\2WZ\3\2\2\2XV\3\2\2")
-        buf.write("\2XY\3\2\2\2Y[\3\2\2\2ZX\3\2\2\2[e\5\f\7\2\\`\7\6\2\2")
-        buf.write("]_\7\n\2\2^]\3\2\2\2_b\3\2\2\2`^\3\2\2\2`a\3\2\2\2ac\3")
-        buf.write("\2\2\2b`\3\2\2\2ce\5\f\7\2dT\3\2\2\2d\\\3\2\2\2e\17\3")
-        buf.write("\2\2\2fg\t\2\2\2g\21\3\2\2\2\22\24\27\36#(/\64;AGLPRX")
-        buf.write("`d")
+        buf.write("\3\3\3\3\3\3\5\3\37\n\3\3\3\5\3\"\n\3\3\4\3\4\3\4\5\4")
+        buf.write("\'\n\4\3\5\7\5*\n\5\f\5\16\5-\13\5\3\5\3\5\7\5\61\n\5")
+        buf.write("\f\5\16\5\64\13\5\7\5\66\n\5\f\5\16\59\13\5\3\5\3\5\7")
+        buf.write("\5=\n\5\f\5\16\5@\13\5\3\5\7\5C\n\5\f\5\16\5F\13\5\3\5")
+        buf.write("\7\5I\n\5\f\5\16\5L\13\5\3\6\3\6\5\6P\n\6\3\7\3\7\6\7")
+        buf.write("T\n\7\r\7\16\7U\3\b\3\b\7\bZ\n\b\f\b\16\b]\13\b\3\b\3")
+        buf.write("\b\3\b\7\bb\n\b\f\b\16\be\13\b\3\b\5\bh\n\b\3\t\3\t\3")
+        buf.write("\t\2\2\n\2\4\6\b\n\f\16\20\2\3\3\2\7\t\2t\2\22\3\2\2\2")
+        buf.write("\4\33\3\2\2\2\6#\3\2\2\2\b+\3\2\2\2\nO\3\2\2\2\fS\3\2")
+        buf.write("\2\2\16g\3\2\2\2\20i\3\2\2\2\22\24\5\b\5\2\23\25\5\6\4")
+        buf.write("\2\24\23\3\2\2\2\24\25\3\2\2\2\25\27\3\2\2\2\26\30\5\4")
+        buf.write("\3\2\27\26\3\2\2\2\27\30\3\2\2\2\30\31\3\2\2\2\31\32\7")
+        buf.write("\2\2\3\32\3\3\2\2\2\33\34\7\3\2\2\34\36\5\b\5\2\35\37")
+        buf.write("\5\6\4\2\36\35\3\2\2\2\36\37\3\2\2\2\37!\3\2\2\2 \"\5")
+        buf.write("\4\3\2! \3\2\2\2!\"\3\2\2\2\"\5\3\2\2\2#$\7\4\2\2$&\5")
+        buf.write("\b\5\2%\'\5\6\4\2&%\3\2\2\2&\'\3\2\2\2\'\7\3\2\2\2(*\7")
+        buf.write("\13\2\2)(\3\2\2\2*-\3\2\2\2+)\3\2\2\2+,\3\2\2\2,\67\3")
+        buf.write("\2\2\2-+\3\2\2\2.\62\5\16\b\2/\61\7\13\2\2\60/\3\2\2\2")
+        buf.write("\61\64\3\2\2\2\62\60\3\2\2\2\62\63\3\2\2\2\63\66\3\2\2")
+        buf.write("\2\64\62\3\2\2\2\65.\3\2\2\2\669\3\2\2\2\67\65\3\2\2\2")
+        buf.write("\678\3\2\2\28:\3\2\2\29\67\3\2\2\2:D\5\f\7\2;=\7\13\2")
+        buf.write("\2<;\3\2\2\2=@\3\2\2\2><\3\2\2\2>?\3\2\2\2?A\3\2\2\2@")
+        buf.write(">\3\2\2\2AC\5\n\6\2B>\3\2\2\2CF\3\2\2\2DB\3\2\2\2DE\3")
+        buf.write("\2\2\2EJ\3\2\2\2FD\3\2\2\2GI\7\13\2\2HG\3\2\2\2IL\3\2")
+        buf.write("\2\2JH\3\2\2\2JK\3\2\2\2K\t\3\2\2\2LJ\3\2\2\2MP\5\16\b")
+        buf.write("\2NP\5\f\7\2OM\3\2\2\2ON\3\2\2\2P\13\3\2\2\2QT\5\20\t")
+        buf.write("\2RT\7\n\2\2SQ\3\2\2\2SR\3\2\2\2TU\3\2\2\2US\3\2\2\2U")
+        buf.write("V\3\2\2\2V\r\3\2\2\2W[\7\5\2\2XZ\7\13\2\2YX\3\2\2\2Z]")
+        buf.write("\3\2\2\2[Y\3\2\2\2[\\\3\2\2\2\\^\3\2\2\2][\3\2\2\2^h\5")
+        buf.write("\f\7\2_c\7\6\2\2`b\7\13\2\2a`\3\2\2\2be\3\2\2\2ca\3\2")
+        buf.write("\2\2cd\3\2\2\2df\3\2\2\2ec\3\2\2\2fh\5\f\7\2gW\3\2\2\2")
+        buf.write("g_\3\2\2\2h\17\3\2\2\2ij\t\2\2\2j\21\3\2\2\2\23\24\27")
+        buf.write("\36!&+\62\67>DJOSU[cg")
         return buf.getvalue()
 
 
@@ -62,8 +63,8 @@ class CommandParser ( Parser ):
     literalNames = [ "<INVALID>", "';'", "'|'", "'<'", "'>'" ]
 
     symbolicNames = [ "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                      "<INVALID>", "UNQUOTED_CONTENT", "SINGLE_QUOTED", 
-                      "BACKQUOTED", "WHITESPACE" ]
+                      "<INVALID>", "SINGLE_QUOTED", "BACKQUOTED", "DOUBLE_QUOTED", 
+                      "UNQUOTED_CONTENT", "WHITESPACE" ]
 
     RULE_command = 0
     RULE_commandSeq = 1
@@ -82,10 +83,11 @@ class CommandParser ( Parser ):
     T__1=2
     T__2=3
     T__3=4
-    UNQUOTED_CONTENT=5
-    SINGLE_QUOTED=6
-    BACKQUOTED=7
-    WHITESPACE=8
+    SINGLE_QUOTED=5
+    BACKQUOTED=6
+    DOUBLE_QUOTED=7
+    UNQUOTED_CONTENT=8
+    WHITESPACE=9
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
@@ -181,12 +183,16 @@ class CommandParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def command(self):
-            return self.getTypedRuleContext(CommandParser.CommandContext,0)
+        def call(self):
+            return self.getTypedRuleContext(CommandParser.CallContext,0)
 
 
         def commandSeq(self):
             return self.getTypedRuleContext(CommandParser.CommandSeqContext,0)
+
+
+        def callPipe(self):
+            return self.getTypedRuleContext(CommandParser.CallPipeContext,0)
 
 
         def getRuleIndex(self):
@@ -218,13 +224,22 @@ class CommandParser ( Parser ):
             self.enterOuterAlt(localctx, 1)
             self.state = 25
             self.match(CommandParser.T__0)
+
             self.state = 26
-            self.command()
+            self.call()
             self.state = 28
             self._errHandler.sync(self)
             _la = self._input.LA(1)
-            if _la==CommandParser.T__0:
+            if _la==CommandParser.T__1:
                 self.state = 27
+                self.callPipe()
+
+
+            self.state = 31
+            self._errHandler.sync(self)
+            _la = self._input.LA(1)
+            if _la==CommandParser.T__0:
+                self.state = 30
                 self.commandSeq()
 
 
@@ -279,15 +294,15 @@ class CommandParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 30
-            self.match(CommandParser.T__1)
-            self.state = 31
-            self.call()
             self.state = 33
+            self.match(CommandParser.T__1)
+            self.state = 34
+            self.call()
+            self.state = 36
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             if _la==CommandParser.T__1:
-                self.state = 32
+                self.state = 35
                 self.callPipe()
 
 
@@ -358,66 +373,66 @@ class CommandParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 38
+            self.state = 41
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==CommandParser.WHITESPACE:
-                self.state = 35
+                self.state = 38
                 self.match(CommandParser.WHITESPACE)
-                self.state = 40
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-
-            self.state = 50
-            self._errHandler.sync(self)
-            _la = self._input.LA(1)
-            while _la==CommandParser.T__2 or _la==CommandParser.T__3:
-                self.state = 41
-                self.redirection()
-                self.state = 45
-                self._errHandler.sync(self)
-                _la = self._input.LA(1)
-                while _la==CommandParser.WHITESPACE:
-                    self.state = 42
-                    self.match(CommandParser.WHITESPACE)
-                    self.state = 47
-                    self._errHandler.sync(self)
-                    _la = self._input.LA(1)
-
-                self.state = 52
+                self.state = 43
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
             self.state = 53
-            self.argument()
-            self.state = 63
             self._errHandler.sync(self)
-            _alt = self._interp.adaptivePredict(self._input,8,self._ctx)
+            _la = self._input.LA(1)
+            while _la==CommandParser.T__2 or _la==CommandParser.T__3:
+                self.state = 44
+                self.redirection()
+                self.state = 48
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+                while _la==CommandParser.WHITESPACE:
+                    self.state = 45
+                    self.match(CommandParser.WHITESPACE)
+                    self.state = 50
+                    self._errHandler.sync(self)
+                    _la = self._input.LA(1)
+
+                self.state = 55
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+
+            self.state = 56
+            self.argument()
+            self.state = 66
+            self._errHandler.sync(self)
+            _alt = self._interp.adaptivePredict(self._input,9,self._ctx)
             while _alt!=2 and _alt!=ATN.INVALID_ALT_NUMBER:
                 if _alt==1:
-                    self.state = 57
+                    self.state = 60
                     self._errHandler.sync(self)
                     _la = self._input.LA(1)
                     while _la==CommandParser.WHITESPACE:
-                        self.state = 54
+                        self.state = 57
                         self.match(CommandParser.WHITESPACE)
-                        self.state = 59
+                        self.state = 62
                         self._errHandler.sync(self)
                         _la = self._input.LA(1)
 
-                    self.state = 60
+                    self.state = 63
                     self.atom() 
-                self.state = 65
+                self.state = 68
                 self._errHandler.sync(self)
-                _alt = self._interp.adaptivePredict(self._input,8,self._ctx)
+                _alt = self._interp.adaptivePredict(self._input,9,self._ctx)
 
-            self.state = 69
+            self.state = 72
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==CommandParser.WHITESPACE:
-                self.state = 66
+                self.state = 69
                 self.match(CommandParser.WHITESPACE)
-                self.state = 71
+                self.state = 74
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
@@ -470,17 +485,17 @@ class CommandParser ( Parser ):
         localctx = CommandParser.AtomContext(self, self._ctx, self.state)
         self.enterRule(localctx, 8, self.RULE_atom)
         try:
-            self.state = 74
+            self.state = 77
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [CommandParser.T__2, CommandParser.T__3]:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 72
+                self.state = 75
                 self.redirection()
                 pass
-            elif token in [CommandParser.UNQUOTED_CONTENT, CommandParser.SINGLE_QUOTED, CommandParser.BACKQUOTED]:
+            elif token in [CommandParser.SINGLE_QUOTED, CommandParser.BACKQUOTED, CommandParser.DOUBLE_QUOTED, CommandParser.UNQUOTED_CONTENT]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 73
+                self.state = 76
                 self.argument()
                 pass
             else:
@@ -541,20 +556,20 @@ class CommandParser ( Parser ):
         self.enterRule(localctx, 10, self.RULE_argument)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 78 
+            self.state = 81 
             self._errHandler.sync(self)
             _alt = 1
             while _alt!=2 and _alt!=ATN.INVALID_ALT_NUMBER:
                 if _alt == 1:
-                    self.state = 78
+                    self.state = 81
                     self._errHandler.sync(self)
                     token = self._input.LA(1)
-                    if token in [CommandParser.SINGLE_QUOTED, CommandParser.BACKQUOTED]:
-                        self.state = 76
+                    if token in [CommandParser.SINGLE_QUOTED, CommandParser.BACKQUOTED, CommandParser.DOUBLE_QUOTED]:
+                        self.state = 79
                         self.quoted()
                         pass
                     elif token in [CommandParser.UNQUOTED_CONTENT]:
-                        self.state = 77
+                        self.state = 80
                         self.match(CommandParser.UNQUOTED_CONTENT)
                         pass
                     else:
@@ -563,9 +578,9 @@ class CommandParser ( Parser ):
 
                 else:
                     raise NoViableAltException(self)
-                self.state = 80 
+                self.state = 83 
                 self._errHandler.sync(self)
-                _alt = self._interp.adaptivePredict(self._input,12,self._ctx)
+                _alt = self._interp.adaptivePredict(self._input,13,self._ctx)
 
         except RecognitionException as re:
             localctx.exception = re
@@ -619,41 +634,41 @@ class CommandParser ( Parser ):
         self.enterRule(localctx, 12, self.RULE_redirection)
         self._la = 0 # Token type
         try:
-            self.state = 98
+            self.state = 101
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [CommandParser.T__2]:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 82
+                self.state = 85
                 self.match(CommandParser.T__2)
-                self.state = 86
+                self.state = 89
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
                 while _la==CommandParser.WHITESPACE:
-                    self.state = 83
+                    self.state = 86
                     self.match(CommandParser.WHITESPACE)
-                    self.state = 88
+                    self.state = 91
                     self._errHandler.sync(self)
                     _la = self._input.LA(1)
 
-                self.state = 89
+                self.state = 92
                 self.argument()
                 pass
             elif token in [CommandParser.T__3]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 90
+                self.state = 93
                 self.match(CommandParser.T__3)
-                self.state = 94
+                self.state = 97
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
                 while _la==CommandParser.WHITESPACE:
-                    self.state = 91
+                    self.state = 94
                     self.match(CommandParser.WHITESPACE)
-                    self.state = 96
+                    self.state = 99
                     self._errHandler.sync(self)
                     _la = self._input.LA(1)
 
-                self.state = 97
+                self.state = 100
                 self.argument()
                 pass
             else:
@@ -677,6 +692,9 @@ class CommandParser ( Parser ):
 
         def SINGLE_QUOTED(self):
             return self.getToken(CommandParser.SINGLE_QUOTED, 0)
+
+        def DOUBLE_QUOTED(self):
+            return self.getToken(CommandParser.DOUBLE_QUOTED, 0)
 
         def BACKQUOTED(self):
             return self.getToken(CommandParser.BACKQUOTED, 0)
@@ -708,9 +726,9 @@ class CommandParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 100
+            self.state = 103
             _la = self._input.LA(1)
-            if not(_la==CommandParser.SINGLE_QUOTED or _la==CommandParser.BACKQUOTED):
+            if not((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << CommandParser.SINGLE_QUOTED) | (1 << CommandParser.BACKQUOTED) | (1 << CommandParser.DOUBLE_QUOTED))) != 0)):
                 self._errHandler.recoverInline(self)
             else:
                 self._errHandler.reportMatch(self)
