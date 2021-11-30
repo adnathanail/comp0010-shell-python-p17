dcc0054eaf566a33e69e54f7f26f7f4a2d426ff9
Alex Nathanail <adnathanail@hotmail.co.uk>
2021-11-30 16:55:00 +0000

    Improve printing of parse tree

--- src/ParsingTesterListener.py
+++ src/ParsingTesterListener.py
@@ -11,40 +11,40 @@ class CustomCommandListener(CommandListener):
     indent = 0
 
     def enterCommand(self, ctx: CommandParser.CommandContext):
-        self.indent += 1
         print(" " * self.indent, "enterCommand")
+        self.indent += 1
 
     def exitCommand(self, ctx: CommandParser.CommandContext):
         self.indent -= 1
         print(" " * self.indent, "exitCommand")
 
     def enterCommandSeq(self, ctx: CommandParser.CommandSeqContext):
-        self.indent += 1
         print(" " * self.indent, "enterCommandSeq")
+        self.indent += 1
 
     def exitCommandSeq(self, ctx: CommandParser.CommandSeqContext):
         self.indent -= 1
         print(" " * self.indent, "exitCommandSeq")
 
     def enterCallPipe(self, ctx: CommandParser.CallPipeContext):
-        self.indent += 1
         print(" " * self.indent, "enterCallPipe")
+        self.indent += 1
 
     def exitCallPipe(self, ctx: CommandParser.CallPipeContext):
         self.indent -= 1
         print(" " * self.indent, "exitCallPipe")
 
     def enterCall(self, ctx: CommandParser.CallContext):
-        self.indent += 1
         print(" " * self.indent, "enterCall")
+        self.indent += 1
 
     def exitCall(self, ctx: CommandParser.CallContext):
         self.indent -= 1
         print(" " * self.indent, "exitCall")
 
     def enterAtom(self, ctx: CommandParser.AtomContext):
-        self.indent += 1
         print(" " * self.indent, "enterAtom")
+        self.indent += 1
 
     def exitAtom(self, ctx: CommandParser.AtomContext):
         self.indent -= 1
@@ -52,36 +52,39 @@ class CustomCommandListener(CommandListener):
 
     def enterArgument(self, ctx: CommandParser.ArgumentContext):
         print(" " * self.indent, "enterArgument")
-        self.indent += 1
         if ctx.quoted():
             for el in ctx.quoted():
                 try:
-                    print(f"QUOTED: {el.getText()}")
-                    print(f"TOKENS: {el.getChild(0)}")
-                    print(f"SINGLE: {el.SINGLE_QUOTED()}")
-                    print(f"DOUBLE: {el.DOUBLE_QUOTED()}")
-                    print(f"BACK: {el.BACKQUOTED()}")
+                    print(" " * (self.indent + 1), f"QUOTED: {el.getText()}")
+                    print(" " * (self.indent + 1), f"TOKENS: {el.getChild(0)}")
+                    print(" " * (self.indent + 1), f"SINGLE: {el.SINGLE_QUOTED()}")
+                    print(" " * (self.indent + 1), f"DOUBLE: {el.DOUBLE_QUOTED()}")
+                    print(" " * (self.indent + 1), f"BACK: {el.BACKQUOTED()}")
                 except Exception as e:
                     pass
 
         if len(ctx.UNQUOTED_CONTENT()) > 0:
-            print(f"UNQUOTED:{ctx.UNQUOTED_CONTENT()[0].getText()}")
+            print(
+                " " * (self.indent + 1),
+                f"UNQUOTED:{ctx.UNQUOTED_CONTENT()[0].getText()}",
+            )
+        self.indent += 1
 
     def exitArgument(self, ctx: CommandParser.ArgumentContext):
         self.indent -= 1
         print(" " * self.indent, "exitArgument")
 
     def enterRedirection(self, ctx: CommandParser.RedirectionContext):
-        self.indent += 1
         print(" " * self.indent, "enterRedirection")
+        self.indent += 1
 
     def exitRedirection(self, ctx: CommandParser.RedirectionContext):
         self.indent -= 1
         print(" " * self.indent, "exitRedirection")
 
     def enterQuoted(self, ctx: CommandParser.QuotedContext):
-        self.indent += 1
         print(" " * self.indent, "enterQuoted")
+        self.indent += 1
 
     def exitQuoted(self, ctx: CommandParser.QuotedContext):
         self.indent -= 1
