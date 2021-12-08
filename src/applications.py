317ea2e5e174eddd55d013deadd5f8aa3908c7c1
Bartosz Grabek <bartoszgrabek7@gmail.com>
2021-12-08 13:46:35 +0100

    Added remaining application skeletons with doc, fixed and improved pwd, ls

--- src/applications.py
+++ src/applications.py
@@ -25,73 +25,83 @@ class UnsafeWrapper(Application):
             return str(err)  # TODO: temporary, waits for I/O
 
 
+class Pwd(Application):
+    """Prints current working directory"""
+
+    def exec(self, args, input, output):
+        # no args, or input, there may be output
+        cwd = os.getcwd() + "\n"
+        return cwd
+
+
 class Cd(Application):
-    """changes current working directory"""
+    """Changes current working directory"""
 
     def exec(self, args, input, output):
         path = args[0]
         cwd = os.getcwd()
         os.chdir(path)
-        cwd_new = os.getcwd()
-        logging.debug(f"Changed: {cwd} to {cwd_new}")
+        logging.debug(f"Changed: {cwd} to {os.getcwd()}")
+
+
+class Ls(Application):
+    """
+    List the content of a directory print a list of files seperated by tabs
+    and followed by newline character. Ignores files and directories
+    whose name start with '.'
+    """
+
+    def exec(self, args: List, input: List, output: List):
+        if len(args) == 0:
+            path = os.getcwd()
+        elif len(args) == 1:
+            path = args[0]  # PATH
+        else:  # > 1
+            raise ValueError("Wrong number of command line arguments")
+        list_of_files = [
+            fname for fname in os.listdir(path) if not (fname.startswith("."))
+        ]
+        if len(list_of_files) > 0:
+            return "\t".join(list_of_files)
+        return ""
 
 
 class Cat(Application):
+    """
+    Concatenates the content of given files and prints it to stdout.
+    """
+
     def exec(self, args, input, output):
         pass
 
 
-class Pwd(Application):
-    def exec(self, args, input, output):
-        # no args, or input, there may be output
-        cwd = os.getcwd() + "\n"
-        if output:
-            f = open(output[0], "w+")
-            f.write(cwd)
-        return cwd
-
-
-class Ls(Application):
-    def exec(self, args: List, input: List, output: List):
-        """
-        List the content of a directory print a list of files seperated by tabs and followed by \n
-        Ignores files and directories whose name start with.
-
-            Parameters:
-                args (List): a list of string flags
-                input (List): a list of string relative paths
-            Returns:
-                tab seperated list of string file and dir names
-        TODO: Should it work only for zero or one arguments provided?
-        """
-        if not (input):  # just ls
-            list_of_files = self._get_list_of_files()
-            output.append("\t".join(list_of_files))
-            # print("\t".join(list_of_files))
-        else:
-            for path in input:
-                # print(f"{path}:")
-                list_of_files = self._get_list_of_files(path)  # TODO: leave for now
-                output.append("\t".join(list_of_files))
-                # print("\t".join(list_of_files))
-
-    def _get_list_of_files(self, path=os.getcwd()):
-        return [fname for fname in os.listdir(path) if not (fname.startswith("."))]
-
-
 class Echo(Application):
+    """
+    Prints its arguments separated by spaces and followed by a newline to stdout.
+    """
+
     def exec(self, args, input, output):
         logging.debug(f"Calling echo: {args} \nIn:{input}\nOut:{output}")
         return " ".join(args)
 
 
 class Head(Application):
+    """
+    Prints the first N lines of a given file or stdin.
+    If there are less than N lines, prints only the existing lines
+    without raising an exception.
+    """
+
     def exec(self, args, input, output):
         pass
 
 
 class Tail(Application):
-    """Extracts the last n lines of the specified file"""
+    """
+    Prints the last N lines of a given file or stdin.
+    If there are less than N lines, prints only the existing lines
+    without raising an exception.
+    """
 
     DEFAULT_NUM_LINES = 10
 
@@ -114,6 +124,51 @@ class Tail(Application):
 
 
 class Grep(Application):
+    """
+    Searches for lines containing a match to the specified pattern.
+    The output of the command is the list of lines.
+    Each line is printed followed by a newline.
+    """
+
+    def exec(self, args, input, output):
+        pass
+
+
+class Cut(Application):
+    """
+    Cuts out sections from each line of a given file or stdin
+    and prints the result to stdout.
+    """
+
+    def exec(self, args, input, output):
+        pass
+
+
+class Find(Application):
+    """
+    Recursively searches for files with matching names.
+    Outputs the list of relative paths, each followed by a newline.
+    """
+
+    def exec(self, args, input, output):
+        pass
+
+
+class Uniq(Application):
+    """
+    Detects and deletes adjacent duplicate lines
+    from an input file/stdin and prints the result to stdout.
+    """
+
+    def exec(self, args, input, output):
+        pass
+
+
+class Sort(Application):
+    """
+    Sorts the contents of a file/stdin line by line and prints the result to stdout.
+    """
+
     def exec(self, args, input, output):
         pass
 
@@ -121,14 +176,18 @@ class Grep(Application):
 class ApplicationFactory:
 
     applications = {
-        "cd": Cd,
-        "cat": Cat,
         "pwd": Pwd,
+        "cd": Cd,
         "ls": Ls,
+        "cat": Cat,
         "echo": Echo,
         "head": Head,
         "tail": Tail,
         "grep": Grep,
+        "cut": Cut,
+        "find": Find,
+        "uniq": Uniq,
+        "sort": Sort,
     }
 
     def create(self, app_name) -> Application:
